aa6b6e0ec8074e7882759b5ec8d16d6cabb34595
yoshitaka <yoshitaka.tanaka.1989@gmail.com>
2023-02-26 17:17:03 +0900

    AdMobを数字管理画面に追加

--- OCHITSUKI/Controller/ProjectTableViewController.swift
+++ OCHITSUKI/Controller/ProjectTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import GoogleMobileAds
 
 class ProjectTableViewController: UITableViewController {
     private var projectArray: Results<OchitsukiDataModel>?
@@ -57,93 +58,118 @@ class ProjectTableViewController: UITableViewController {
     
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return schedules.keys.count
+        return schedules.keys.count + 1
     }
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        let targetDate = dateOrder[section]
-        return schedules[targetDate, default: []].count
+        if section < dateOrder.count {
+            let targetDate = dateOrder[section]
+            return schedules[targetDate, default: []].count
+        } else {
+            return 1
+        }
     }
-    
+
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let targetMonth = dateOrder[section]
-        
         let label = UILabel()
         label.backgroundColor = .buttonColor
         label.textColor = .wordColor
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy年MM月"
-        let orderMonth = "\(formatter.string(from: targetMonth))"
-        label.text = "  " + orderMonth
-        
+        if section < dateOrder.count {
+            let targetMonth = dateOrder[section]
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy年MM月"
+            let orderMonth = "\(formatter.string(from: targetMonth))"
+            label.text = "  " + orderMonth
+        } else {
+            label.text = "  " + "広告"
+        }
         return label
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ProjectCell", for: indexPath) as! ProjectCell
-        
-        let targetDate = dateOrder[indexPath.section]
-        guard let targetProjects = schedules[targetDate] else { return cell }
-        guard indexPath.row < targetProjects.count else { return cell }
-        let project = targetProjects[indexPath.row]
-        
-        let name = project.title
-        let value = project.orderAmountUnit
-        let profit = project.grossProfitUnit
-        
-        let date = Date(timeIntervalSince1970: project.orderDate)
-        
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy年MM月dd日"
-        let dateorder = "\(formatter.string(from: date))"
-        
-        cell.projectLaber.text = name
-        cell.orderAmountLaber.text = value
-        cell.grossProfitLabel.text = profit
-        cell.dateLaber.text = dateorder
-        
-        cell.selectionStyle = .none
-        return cell
+        if indexPath.section < dateOrder.count {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ProjectCell", for: indexPath) as! ProjectCell
+
+            let targetDate = dateOrder[indexPath.section]
+            guard let targetProjects = schedules[targetDate] else { return cell }
+            guard indexPath.row < targetProjects.count else { return cell }
+            let project = targetProjects[indexPath.row]
+
+            let name = project.title
+            let value = project.orderAmountUnit
+            let profit = project.grossProfitUnit
+
+            let date = Date(timeIntervalSince1970: project.orderDate)
+
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy年MM月dd日"
+            let dateorder = "\(formatter.string(from: date))"
+
+            cell.projectLaber.text = name
+            cell.orderAmountLaber.text = value
+            cell.grossProfitLabel.text = profit
+            cell.dateLaber.text = dateorder
+            cell.selectionStyle = .none
+            return cell
+        } else {
+            let cell = UITableViewCell()
+            let adSize = GADAdSizeFromCGSize(CGSize(width: 320, height: 50))
+            let bannerView = GADBannerView(adSize: adSize)
+            bannerView.adUnitID = Bundle.main.object(forInfoDictionaryKey: "AdMobSettingViewBannerAdUnitId") as? String
+            bannerView.rootViewController = self
+            bannerView.load(GADRequest())
+            cell.contentView.addSubview(bannerView)
+            bannerView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                bannerView.topAnchor.constraint(equalTo: cell.contentView.topAnchor),
+                bannerView.leadingAnchor.constraint(equalTo: cell.contentView.leadingAnchor),
+                bannerView.trailingAnchor.constraint(equalTo: cell.contentView.trailingAnchor),
+                bannerView.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor)
+            ])
+            return cell
+        }
     }
     
     override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        
-        let contextItem1 = UIContextualAction(style: .destructive, title: DELETE) { (contextualAction, view, boolValue) in
-            let targetDate = self.dateOrder[indexPath.section]
-            guard let targetProjects = self.schedules[targetDate] else { return }
-            guard indexPath.row < targetProjects.count else { return }
-            let project = targetProjects[indexPath.row]
-            self.delete(element: project)
-        }
-        
-        let contextItem2 = UIContextualAction(style: .normal, title: EDIT) {  (contextualAction, view, boolValue) in
-            self.tableView.isEditing = false
-            let targetDate = self.dateOrder[indexPath.section]
-            guard let targetProjects = self.schedules[targetDate] else { return }
-            guard indexPath.row < targetProjects.count else { return }
-            self.editingData = targetProjects[indexPath.row]
-            self.performSegue(withIdentifier: "ToEditModal", sender: self)
-        }
-        let contextItem3 = UIContextualAction(style: .normal, title: DUPLICATE) {  (contextualAction, view, boolValue) in
-            let targetDate = self.dateOrder[indexPath.section]
-            guard let targetProjects = self.schedules[targetDate] else { return }
-            guard indexPath.row < targetProjects.count else { return }
-            let project = targetProjects[indexPath.row]
-            self.duplicate(element: project)
-        }
-        
-        let swipeActions = UISwipeActionsConfiguration(actions: [contextItem1, contextItem2, contextItem3])
+        if indexPath.section < dateOrder.count {
+            let contextItem1 = UIContextualAction(style: .destructive, title: DELETE) { (contextualAction, view, boolValue) in
+                let targetDate = self.dateOrder[indexPath.section]
+                guard let targetProjects = self.schedules[targetDate] else { return }
+                guard indexPath.row < targetProjects.count else { return }
+                let project = targetProjects[indexPath.row]
+                self.delete(element: project)
+            }
 
-        return swipeActions
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
-        if tableView.isEditing {
-            return .delete
+            let contextItem2 = UIContextualAction(style: .normal, title: EDIT) {  (contextualAction, view, boolValue) in
+                self.tableView.isEditing = false
+                let targetDate = self.dateOrder[indexPath.section]
+                guard let targetProjects = self.schedules[targetDate] else { return }
+                guard indexPath.row < targetProjects.count else { return }
+                self.editingData = targetProjects[indexPath.row]
+                self.performSegue(withIdentifier: "ToEditModal", sender: self)
+            }
+            let contextItem3 = UIContextualAction(style: .normal, title: DUPLICATE) {  (contextualAction, view, boolValue) in
+                let targetDate = self.dateOrder[indexPath.section]
+                guard let targetProjects = self.schedules[targetDate] else { return }
+                guard indexPath.row < targetProjects.count else { return }
+                let project = targetProjects[indexPath.row]
+                self.duplicate(element: project)
+            }
+
+            let swipeActions = UISwipeActionsConfiguration(actions: [contextItem1, contextItem2, contextItem3])
+
+            return swipeActions
+        } else {
+            return nil
         }
-        return .none
+    }
+
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if indexPath.section < dateOrder.count {
+            return tableView.isEditing ? .delete : .none
+        } else {
+            return .none
+        }
     }
     
     private func loadDataFromRealm() {
